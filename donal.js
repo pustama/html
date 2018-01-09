6a6370387bf0ebcee7bbd2d6c72a9d2b66f2f414
pustama <pustama@users.noreply.github.com>
2018-01-09 10:07:48 +0700

    Add files via upload

---
+++ donal.js
@@ -0,0 +1,52 @@
+<div class='modal-window1' id='popup'>
+<div>
+<a class='popup-close' href='#popup' title='Close'>close &#215;</a>
+    <br/>
+<h1 class='modal-window1'>
+  <a href='#'>Radikal? Ustaz Abdul Somad yang Saya Kenal</a></h1>
+<div>
+<a href='#'><img class='modal-window1' src='https://2.bp.blogspot.com/-5FVYOZxumek/WkMG2fGbkCI/AAAAAAAAEwo/C-YAUYBneUwp6Jq6G0c9vKVQ6xaZ8T_0gCLcBGAs/s1600/shomad%2Bshamsi.png'/></a>
+Saya sangat kecewa dengan tindakan atau kebijakan sebagian pihak, baik dalam negeri maupun luar negeri, terhadap Ust. Abdul Somad. Kejadian Bali beberapa hari lalu, dan kejadian Hongkong juga tiga hari lalu sangat bertentangan dengan nilai-nilai yang dibanggakan oleh dunia, khususnya yang menganut paham demokrasi dengan kebebasan (freedom) sebagai esensi dasarnya.<br />
+<br />
+Apapun alasannya pencekalan itu baik oleh oknum masyarakat maupun pemerintah sangat tidak sejalan dengan semangat kebebasan dan keragaman.<br />
+<br />
+Dalam dunia demokrasi yang kita kenal, seperti Amerika, kebebasan ekspresi, berbicara dan berpendapat itu hak asasi manusia yang sangat dihormati. Dan tidak akan dianggap ancaman selama itu masih dalam batas opini atau bicara (speech). Maka mengeritik presiden sekalipun adalah hal wajar selama tidak ada ancaman, apalagi tindakan kekerasan (violence).<br />
+<br />
+Sejujurnya saya belum terlalu lama mengenal Ust. Abdul Somad, MA. Baru sekitar bulan Juli lalu saya kembali ke Jakarta dan salah seorang pengurus Islamic Center Jakarta memberikan usulan agar ceramah-ceramah saya direkam. Menurutnya ada seorang Ust. saat ini yang menjadi magnet, disenangi di mana-mana. Beliaupun menyebutkan nama itu, Ust. Abdul Somad.<br />
+<br />
+Dari situlah saya menjadi ingin tahu siapa gerangan sosok itu. Saya cari beliau di Youtube, dan subhanallah, beliau adalah sosok ulama yang luar biasa. Saya tidak terlalu tertarik dengan kepopulerannya. Karena bintang film juga banyak yang populer, politisi juga demikian. Bagi saya banyak cara untuk populer. Teroris juga populer karena kejahatannya.<br />
+<br />
+Justeru yang membuat saya terkagum dengan beliau adalah keseimbangan dalam memahami Islam. Mungkin bahasa populernya beliau sangat moderat dalam pemahaman. Tidak ekstrim ke samping mana saja, baik kiri maupun kanan.<br />
+<br />
+Tapi yang lebih penting adalah keluasan ilmu dan referensi agama yang beliau miliki. Sungguh saya senang ketika seseorang beragumentasi dan menyampaikan argumentasi dengan referensi dan pemahaman yang luas.<br />
+<br />
+Maka ceramah-ceramah yang diselingi dengan humor-humor yang sesuai dan mengena bagi saya memang sangat wajar jika memiliki daya atraksi yang tinggi. Sehingga di mana-mana beliau dicari, diterima oleh massa yang sangat besar.<br />
+<br />
+Karakter mulia<br />
+<br />
+Tapi yang paling saya kagumi dari guru kita ini adalah kesederhanaan, apa adanya, dan insya Allah mengatakan dan melakukan semuanya tanpa dipoles-poles. Beliau melakukan dakwah tampa pilih, di kota besar atas undangan pejabat besar atau dipelosok desa atas undangan rakyat kecil. Bagi beliau semuanya punya hak yang sama.<br />
+<br />
+Melalui berbagai ceramah di youtube, saya diam-diam memang jatuh hati baik oleh pemikiran dan pendapat keagamaan beliau maupun cara penyampaian yang berkarakter dan menyegarkan. Bahkan jujur saya banyak menimba ilmu baru dan segar dari ceramah-ceramah beliau.<br />
+<br />
+Oleh karenanya saya memutuskan untuk mengundang beliau. Saya mencari kontak beliau dan mengontak beliau. Subhanallah beliau dengan sangat hormat dan lapang dada merespon dengan positif undangan kami ke US insya Allah.<br />
+<br />
+Keinginan saya untuk mengundang beliau adalah selain memberikan tausiah-tausiah ke masyarakat Indonesia di Amerika, juga seorang ustadz, apalagi sebesar nama beliau perlu diberikan akses global. Bahwa keluasan ilmu agama menjadi sangat penting untuk dibarengi oleh pengalaman yang lebih banyak sehingga wawasan akan semakin menjadi luas pula.<br />
+<br />
+Maka saya berusaha menemui beliau di saat ada kesempatan kembali ke tanah air. Kesempatan itupun terjadi di tempat Ustaz Arifin Ilham, Sentul. Subhallah saya menemukan sosok yang luar biasa dalam kesederhanaan, kesahajaan, tapi memiliki kharisma dalam kata dan penyampaian.<br />
+<br />
+Dua hari setelah itu kembali kami dipertemukan di kampung halaman saya di Makassar. Saya sungguh kagum betapa beliau dikarunia Allah kemampuan keilmuan dan daya tarik sehingga massa begitu berlimpah untuk mendengarkan tausiah-tausiah beliau.<br />
+<br />
+Maka mendengarkan berita penolakan dan pencekalan itu menjadikan saya kecewa, entah kepada siapa. Walau saya sadar bahwa da&#8217;wah itu alamiahnya pasti akan tertantang. Saya bukan mempermasalahkan itu. Tapi mereka yang menolak atau mencekal dengan tuduhan-tuduhan yang jahat, bahkan tanpa ada bukti.<br />
+<br />
+Sempurnahkan Ustaz Abdul Somad? Apakah beliau bersih sama sekali dari kekurangan dan kesalahan?<br />
+<br />
+Kata orang Amerika, who the hell is perfect? Siapa yang sempurna?<br />
+<br />
+Tapi jika seorang Abdul Somad yang sopan, santun, imbang dan moderat, menghormati perbedaan, cinta sesama Muslim dan sesama manusia, dan cinta damai dicekal? Lalu siapa lagi yang dianggap tidak radikal? Apakah menyampaikan Islam dengan jujur dan apa adanya itu radikal?<br />
+<br />
+Kalau ternyata radikalisme itu dipahami demikian, maka saya mengatakan tidak setuju dengan konsep moderasi. Jika moderasi berarti menyembunyikan kebenaran, mengesampingkan keadilan, maka masanya kita perlu merumuskan konsep moderasi yang berbeda. Wallahu a&#8217;lam!<br />
+<br />
+Oleh: Imam Shamsi Ali di <a href="http://www.republika.co.id/berita/jurnalisme-warga/wacana/17/12/26/p1jz2s319-radikal-ustaz-abdul-somad-yang-saya-kenal" rel="nofollow" target="_blank">republika</a><br />
+Denver, 25 Desember 2017.</div>
+
+</div></div>
